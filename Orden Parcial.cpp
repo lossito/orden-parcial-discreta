a74b2ba9f69815cf84341feb0ddbdc2ce38f4462
Piero André Burgos Duncan <u202513368@upc.edu.pe>
2026-04-12 18:07:23 -0500

    v143

--- Orden Parcial.cpp
+++ Orden Parcial.cpp
@@ -1,12 +1,13 @@
 #include "operaciones.h"
+#include <conio.h>
 int main() {
 	int opc = 0;
 	while (opc != 2)
 	{
+		system("cls");
 		cout << "--Bienvenido al mvp del tb1--\n";
 		cout << "1. Empezar orden parcial\n";
 		cout << "2. Salir\n";
-		cout << "3. Contactanos en:\n";
 		cout << "Opcion: ";
 		cin >> opc;
 		switch (opc) {
@@ -14,17 +15,13 @@ int main() {
 				int n = 0;
 				cout << "Ingrese un numero entero: ";
 				cin >> n;
-				vector<int> generarDivisores(n);
-				imprimirDivisores(n, generarDivisores);
+				vector<int> aux = generarDivisores(n);
+				imprimirDivisores(aux);
+				_getch();
+				cout << "\n";
 				break;
 			}
 			case 2: {
-				break;
-			}
-			case 3: {
-				for (int i = 0; i < 9999; i++) {
-					cout << "contactofidel.ucv.edu.pe\n";
-				}
 				break;
 			}
 			default: {
--- operaciones.h
+++ operaciones.h
@@ -4,7 +4,8 @@ using namespace std;
 
 vector<int> generarDivisores(int n) {
     vector<int> divisores;
-    for (int i = 1; i <= n - 1 ; i++) {
+    for (int i = 1; i <= n - 1; i++)
+    {
         if (n % i == 0) {
             divisores.push_back(i);
         }
@@ -12,14 +13,13 @@ vector<int> generarDivisores(int n) {
     return divisores;
 }
 
-auto Yunzu = [](int A, int C) {};
-
 //vector<int> crearSubconjunto(int n, vector<int> divisores) {}
 
-void imprimirDivisores(int n, vector<int> divisores) {
+void imprimirDivisores(vector<int> divisores) {
     cout << "D = { ";
-    for (int i = 1; i <= n - 1; i++) {
-        cout << divisores[i] << " ; ";
-    }
+    for (auto i : divisores)
+    {
+        cout << i << " ; ";
+    } 
     cout << " } ";
 }
